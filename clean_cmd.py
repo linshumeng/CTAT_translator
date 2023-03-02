db4c62ed8fd9e42eba95930fff4ef396f7e3e123
Shumeng Lin <54089164+linshumeng@users.noreply.github.com>
2023-03-02 11:12:30 -0500

    Update clean function running from cmd

--- clean_cmd.py
+++ clean_cmd.py
@@ -2,153 +2,189 @@ import xml.etree.ElementTree as ET
 import re
 from nltk.corpus import stopwords
 import pandas as pd
+import glob
+from tqdm import tqdm
+from xmldiff import main
+import os
+import translators.server as tss
 import sys
 
 STOPWORDS = stopwords.words('english')
 
-# create name for variable
-def clean_name(s, convert_to_lower=True):
-    s = re.sub('<[^<]+?>', '', s) # markup
-    s = re.sub('[^0-9a-zA-Z_\s]', '', s) # keep alnum
-    s = re.sub('\t\n\r', '', s) # remove tab, line break, carriage return
-    s = ' '.join(s.split()) # remove redundant whitespace
-    return s.lower() if convert_to_lower else s
+class clean():
+    """the class for cleaning purpose"""
+    def __init__(self, infile_brd, infile_table, outfile_brd, outfile_table):
+        self.infile_brd = infile_brd
+        self.infile_table = infile_table
+        self.outfile_brd = outfile_brd
+        self.outfile_table = outfile_table
+        self.var_phrase_map = dict()
+        self.var_name_map = dict()
+        self.table_new = None
+        self.table_new_index_list = None
 
-# remove unnecessary part in value
-def clean_phrase(s, convert_to_lower=False):
-    if(s is None or s == ''):
-        return None
-    s = re.sub('\t\n\r', '', s) # remove tab, line break, carriage return
-    s = ' '.join(s.split()) # remove redundant whitespace
-    return s.lower() if convert_to_lower else s
+    def clean_name(self, s, convert_to_lower=True):
+        """create name for variables in .brd"""
+        s = re.sub('<[^<]+?>', '', s) # markup
+        s = re.sub('[^0-9a-zA-Z_\s]', '', s) # keep alnum
+        s = re.sub('\t\n\r', '', s) # remove tab, line break, carriage return
+        s = ' '.join(s.split()) # remove redundant whitespace
+        return s.lower() if convert_to_lower else s
+    
+    def clean_phrase(self, s, convert_to_lower=False):
+        """remove unnecessary part in value"""
+        if(s is None or s == ''):
+            return None
+        s = re.sub('\t\n\r', '', s) # remove tab, line break, carriage return
+        s = ' '.join(s.split()) # remove redundant whitespace
+        return s.lower() if convert_to_lower else s
 
-# create variable
-def make_var(phrase, signature='_', keep_n_words=4):
-    if (phrase is None or phrase == ''):
-        return ''
-    the_clean_phrase = clean_phrase(phrase) 
-    # print(the_clean_phrase)
-    if the_clean_phrase in var_phrase_map: # if the variable in var_phrase_map
-        return var_phrase_map[the_clean_phrase]
-    else:
-        h = signature + '_' + '_'.join([word for word in clean_name(phrase).split(' ') if word not in STOPWORDS][:keep_n_words])
-        v = '%(' + str(h) + ')%' # create variable name
-        var_phrase_map[the_clean_phrase] = v # dict key: variable value, dict value: variable name
-        return v
-
-# change variable name
-def change_var(old_name, signature='_', keep_n_words=4):
-    if old_name in var_name_map:
-        return var_name_map[old_name]
-    else:
-        phrase = table_new.loc[old_name].iloc[0] # find the pharse in the mass production table
-        if (old_name is None or old_name == '' or pd.isnull(phrase)):
-            return ''
-        the_clean_phrase = clean_phrase(phrase) 
-        h = signature + '_' + '_'.join([word for word in clean_name(phrase).split(' ') if word not in STOPWORDS][:keep_n_words])
-        v = '%(' + str(h) + ')%'
-        var_name_map[old_name] = v
-        table_new.rename(index={old_name:v}, inplace=True) # dict key: variable value, dict value: variable name
-        return v
-
-# find hash
-def find_hash(s):
-    if(s is None or s == ''):
-        return False
-    # replace "%(" and "%)" to detect whether the variable name is a hash-like
-    s = re.sub('%\(', '', s) # "\" is for re
-    s = re.sub('\)%', '', s)
-    return s.lstrip('-').isdigit()
-
-# replace pharse with variable
-def iterate_generic(tag: str, root):
-    """txt should be %% type or a pharse."""
-    table_new_index_list = table_new.index.tolist() 
-    count = 1
-    for element in root.iter(tag):
-        if tag == 'Input' and element[0].tag == 'value': # find input value
-            txt = element[0].text
+    def find_hash(self, s):
+        """find hash-like variable"""
+        if(s is None or s == ''):
+            return False
+        # replace "%(" and "%)" to detect whether the variable is a hash-like
+        s = re.sub('%\(', '', s) # "\(" is for re to search "("
+        s = re.sub('\)%', '', s)
+        return s.lstrip('-').isdigit() # ignore "-" in the variable 
+    
+    def change_var(self, old_name, signature='_', keep_n_words=4):
+        """change hash-like variable's name in df"""
+        if old_name in self.var_name_map:
+            return self.var_name_map[old_name]
         else:
-            txt = element.text
+            phrase = self.table_new.loc[old_name].iloc[0] # find the first pharse in the mass production table
+            if (old_name is None or old_name == '' or pd.isnull(phrase)):
+                return ''
+            the_clean_phrase = self.clean_phrase(phrase) 
+            h = signature + '_' + '_'.join([word for word in self.clean_name(the_clean_phrase).split(' ') if word not in STOPWORDS][:keep_n_words])
+            v = '%(' + str(h) + ')%'
+            self.var_name_map[old_name] = v
+            self.table_new.rename(index={old_name:v}, inplace=True) # dict key: variable value, dict value: variable name
+            return v
+    
+    def make_var(self, phrase, signature='_', keep_n_words=4):
+        """create variable-value pair"""
+        if (phrase is None or phrase == ''):
+            return ''
+        the_clean_phrase = self.clean_phrase(phrase) # clean the value(phrase)
+        # if the variable in self.var_phrase_map
+        if the_clean_phrase in self.var_phrase_map: 
+            return self.var_phrase_map[the_clean_phrase]
+        # else create one
+        else:
+            h = signature + '_' + '_'.join([word for word in self.clean_name(the_clean_phrase).split(' ') if word not in STOPWORDS][:keep_n_words])
+            v = '%(' + str(h) + ')%' # create variable name
+            self.var_phrase_map[the_clean_phrase] = v # dict key: value, dict value: variable
+            return v
+        
+    def process_txt(self, txt, element, tag, count):
+        """process txt"""
         # if txt is empty
-        if clean_phrase(txt) is None or clean_phrase(txt) == '':
-            continue
+        if self.clean_phrase(txt) is None or self.clean_phrase(txt) == '':
+            return
         # elif txt is already in the mass production table and it is not a hash-like
-        elif txt in table_new_index_list and find_hash(txt) is False:
-            continue
+        elif txt in self.table_new_index_list and self.find_hash(txt) is False:
+            return
         # elif txt is already in the mass production table and it is a hash-like
-        elif txt in table_new_index_list and find_hash(txt) is True:
+        elif txt in self.table_new_index_list and self.find_hash(txt) is True:
             if tag == 'Input':
-                element[0].text = change_var(txt, signature=tag+'_'+str(count))
+                element[0].text = self.change_var(txt, signature=tag+'_'+str(count))
             else:
-                element.text = change_var(txt, signature=tag+'_'+str(count))
+                element.text = self.change_var(txt, signature=tag+'_'+str(count))
+            return
         # else create a variable name for the value
         else:
             if tag == 'Input':
-                element[0].text = make_var(txt, signature=tag+'_'+str(count))
+                element[0].text = self.make_var(txt, signature=tag+'_'+str(count))
             else:
-                element.text = make_var(txt, signature=tag+'_'+str(count))
-        count += 1
+                element.text = self.make_var(txt, signature=tag+'_'+str(count))
+            return 
+    
+    def iterate_generic(self, tag: str, root):
+        """replace pharse with variable,
+            txt should be %% type or a pharse"""
+        count = 1
+        for element in root.iter(tag):
+            # print(tag)
+            if tag == 'Input' and element[0].tag == 'value': # find input value
+                txt = element[0].text
+                self.process_txt(txt, element, tag, count)
+            else:
+                txt = element.text
+                self.process_txt(txt, element, tag, count)
+            count += 1
 
-# read the tags and call all functions above
-def process_file(infile_brd, infile_table, outfile_brd, outfile_table):
-    global tree; tree = ET.parse(infile_brd)
-    print("mass production brd input read")
-    print("path: " + infile_brd)
-    root = tree.getroot()
-    global var_phrase_map; var_phrase_map = dict()
-    global var_name_map; var_name_map = dict()
+    def clean_file(self):
+        """read the tags and call all functions above"""
+        tree = ET.parse(self.infile_brd)
+        print("mass production brd input read")
+        print("path: " + self.infile_brd)
+        root = tree.getroot()
 
-    global table_new; table_new = pd.read_csv(infile_table, sep="\t", index_col=0)
-    print("mass production table input read")
-    print("path: " + infile_table)
+        self.table_new = pd.read_csv(self.infile_table, sep="\t", index_col=0, keep_default_na=False)
+        self.table_new_index_list = self.table_new.index.tolist()
+        print("mass production table input read")
+        print("path: " + self.infile_table)
 
-    tags = ['hintMessage', 'buggyMessage', 'successMessage', 
-            'label', 'Input']
-    for tag in tags:
-        iterate_generic(tag, root) 
+        tags = ['hintMessage', 'successMessage', 'buggyMessage', 'label', 'Input']
+        for tag in tags:
+            self.iterate_generic(tag, root) 
 
-    # create new dataframe and concat it with the latest mass production table
-    df_new = pd.DataFrame(var_phrase_map.keys(), index = list(var_phrase_map.values()))
-    df_dup = pd.concat([df_new.T]*len(table_new.columns)).T
-    df_dup.columns = table_new.columns
-    df_mix = pd.concat([table_new, df_dup])
+        # create new dataframe and concat it with the latest mass production table
+        df_new = pd.DataFrame(self.var_phrase_map.keys(), index = list(self.var_phrase_map.values()))
+        df_dup = pd.concat([df_new.T]*len(self.table_new.columns)).T
+        df_dup.columns = self.table_new.columns
+        df_mix = pd.concat([self.table_new, df_dup])
+        df_mix.index.name = self.table_new.index.name
 
-    # export the csv
-    df_mix.to_csv(outfile_table, encoding="utf-8", sep="\t")
-    print("mass production table output finished")
-    print("path: " + outfile_table)
+        # export the csv
+        df_mix.to_csv(self.outfile_table, encoding="utf-8", sep="\t")
+        print("mass production table output finished")
+        print("path: " + self.outfile_table)
 
-    # export the brd
-    tree.write(outfile_brd)
-    print("mass production brd output finished")
-    print("path: " + outfile_table)
+        # export the brd
+        output = open(self.outfile_brd, 'w+b')
+        output.write(b'<?xml version="1.0" standalone="yes"?>\n\n')
+        tree.write(output)
+        print("mass production brd output finished")
+        print("path: " + self.outfile_table)
 
-    return
+        return self.table_new, df_mix
 
-# read the latest mass production graph and mass production table
-try:
-    infile_brd =sys.argv[1]
-    print("using argument 1 as brd input")
-except:
-    infile_brd = "./HTML_folder/7.17 HTML/7.17 HTML/MassProduction/7-17_finalTemplate_new.brd"
-try:
-    infile_table = sys.argv[2]
-    print("using argument 2 as table input")
-except:
-    infile_table = "./HTML_folder/7.17 HTML/7.17 HTML/MassProduction/7-17_finalMassProduction_new.txt"
+if __name__ == "__main__":
+    # run the process function
+    print("clean task ------")
+    html_brd = sys.argv[1]
+    html_table = sys.argv[2]
+    clean_brd = sys.argv[3]
+    clean_table = sys.argv[4]
+    clean_res = clean(html_brd, html_table, clean_brd, clean_table)
+    _, _ = clean_res.clean_file()
 
-# set the output paths
-try:
-    outfile_brd = sys.argv[3]
-    print("using argument 3 as brd output")
-except:
-    outfile_brd = infile_brd.replace('/HTML_folder/7.17 HTML/7.17 HTML/MassProduction/', '/Output_cleaned_folder/').replace('.brd', '_cleaned.brd')
-try:
-    outfile_table = sys.argv[4]
-    print("using argument 4 as table output")
-except:
-    outfile_table = infile_table.replace('/HTML_folder/7.17 HTML/7.17 HTML/MassProduction/', '/Output_cleaned_folder/').replace('.txt', '_cleaned.txt')
+# # read the latest mass production graph and mass production table
+# try:
+#     infile_brd =sys.argv[1]
+#     print("using argument 1 as brd input")
+# except:
+#     infile_brd = "./HTML_folder/7.17 HTML/7.17 HTML/MassProduction/7-17_finalTemplate_new.brd"
+# try:
+#     infile_table = sys.argv[2]
+#     print("using argument 2 as table input")
+# except:
+#     infile_table = "./HTML_folder/7.17 HTML/7.17 HTML/MassProduction/7-17_finalMassProduction_new.txt"
 
-# run the process function
-process_file(infile_brd, infile_table, outfile_brd, outfile_table)
+# # set the output paths
+# try:
+#     outfile_brd = sys.argv[3]
+#     print("using argument 3 as brd output")
+# except:
+#     outfile_brd = infile_brd.replace('/HTML_folder/7.17 HTML/7.17 HTML/MassProduction/', '/Output_cleaned_folder/').replace('.brd', '_cleaned.brd')
+# try:
+#     outfile_table = sys.argv[4]
+#     print("using argument 4 as table output")
+# except:
+#     outfile_table = infile_table.replace('/HTML_folder/7.17 HTML/7.17 HTML/MassProduction/', '/Output_cleaned_folder/').replace('.txt', '_cleaned.txt')
+
+# # run the process function
+# process_file(infile_brd, infile_table, outfile_brd, outfile_table)
